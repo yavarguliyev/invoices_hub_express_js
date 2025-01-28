6fd164b57736a108d2fee114d4dae180d1ee8df5
yavarguliyev <yavarrg@code.edu.az>
2025-01-28 14:50:25 +0400

    redundant try/catch blocks removed

--- src/controllers/v1/healthcheck.controller.ts
+++ src/controllers/v1/healthcheck.controller.ts
@@ -3,7 +3,6 @@ import { JsonController, Get } from 'routing-controllers';
 import RedisInfrastructure from 'infrastructure/redis.infrastructure';
 import RabbitMQInfrastructure from 'infrastructure/rabbitmq.infrastructure';
 import { DbConnectionInfrastructure } from 'infrastructure/db-connection.infrastructure';
-import { DatabaseConnectionError } from 'errors';
 import { createVersionedRoute } from 'helpers/utility-functions.helper';
 
 @JsonController(createVersionedRoute('/healthcheck', 'v1'))
@@ -12,18 +11,14 @@ export class HealthcheckController {
   async healthcheck () {
     const healthcheckStatus = { message: 'OK', uptime: process.uptime(), timestamp: Date.now(), redis: 'unhealthy', rabbitMQ: 'unhealthy', db: 'unhealthy' };
 
-    try {
-      const db = DbConnectionInfrastructure.isConnected();
-      const redis = await RedisInfrastructure.isConnected();
-      const rabbitMQ = RabbitMQInfrastructure.isConnected();
+    const db = DbConnectionInfrastructure.isConnected();
+    const redis = await RedisInfrastructure.isConnected();
+    const rabbitMQ = RabbitMQInfrastructure.isConnected();
 
-      healthcheckStatus.db = db ? 'healthy' : 'unhealthy';
-      healthcheckStatus.redis = redis ? 'healthy' : 'unhealthy';
-      healthcheckStatus.rabbitMQ = rabbitMQ ? 'healthy' : 'unhealthy';
+    healthcheckStatus.db = db ? 'healthy' : 'unhealthy';
+    healthcheckStatus.redis = redis ? 'healthy' : 'unhealthy';
+    healthcheckStatus.rabbitMQ = rabbitMQ ? 'healthy' : 'unhealthy';
 
-      return healthcheckStatus;
-    } catch (error) {
-      throw new DatabaseConnectionError({ operation: 'healthcheck', error });
-    }
+    return healthcheckStatus;
   }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,5 @@
 import { Container } from 'typedi';
 import { plainToInstance } from 'class-transformer';
-import { EntityNotFoundError } from 'typeorm';
 
 import { EVENTS } from 'value-objects/enums/events.enum';
 import { REDIS_CACHE_KEYS } from 'value-objects/types/redis/redis-decorator.types';
@@ -15,7 +14,7 @@ import { DeleteUserArgs } from 'value-objects/inputs/user/delete-user.args';
 import { UserResultsDto } from 'value-objects/dto/user/user-results.dto';
 import { UserResultMessage } from 'value-objects/enums/user-result-message.enum';
 import { UserDto } from 'value-objects/dto/user/user.dto';
-import { NotFoundError, BadRequestError, DatabaseConnectionError } from 'errors';
+import { NotFoundError, BadRequestError } from 'errors';
 
 export interface IUserService {
   get(): Promise<UserResultsDto>;
@@ -36,105 +35,73 @@ export class UserService implements IUserService {
 
   @RedisDecorator({ keyTemplate: REDIS_CACHE_KEYS.USER_GET_LIST })
   async get (): Promise<UserResultsDto> {
-    try {
-      const users = await this.userRepository.find();
-      const userDtos = users.map((user) => plainToInstance(UserDto, user, { excludeExtraneousValues: true }));
+    const users = await this.userRepository.find();
+    const userDtos = users.map((user) => plainToInstance(UserDto, user, { excludeExtraneousValues: true }));
 
-      return { users: userDtos, result: UserResultMessage.SUCCEED };
-    } catch (error) {
-      throw new DatabaseConnectionError({ operation: 'find', error });
-    }
+    return { users: userDtos, result: UserResultMessage.SUCCEED };
   }
 
   async getBy ({ id }: GetUserArgs): Promise<UserResultsDto> {
-    try {
-      const user = await this.userRepository.findOneByOrFail({ id });
-      const userDto = plainToInstance(UserDto, user, { excludeExtraneousValues: true });
-
-      return { user: userDto, result: UserResultMessage.SUCCEED };
-    } catch (error) {
-      if (error instanceof EntityNotFoundError) {
-        throw new NotFoundError(`User with ID ${id} not found.`, { id });
-      }
-
-      throw new DatabaseConnectionError({ operation: 'findOneByOrFail', error });
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundError(`User with ID ${id} not found.`, { id });
     }
+
+    const userDto = plainToInstance(UserDto, user, { excludeExtraneousValues: true });
+
+    return { user: userDto, result: UserResultMessage.SUCCEED };
   }
 
   @EventPublisherDecorator({ keyTemplate: REDIS_CACHE_KEYS.USER_GET_LIST, event: EVENTS.USER_CREATED })
   async create (userData: CreateUserArgs): Promise<UserResultsDto> {
     const { email, roleId } = userData;
 
-    try {
-      const existingUser = await this.userRepository.findOne({ where: { email }, withDeleted: true });
-      if (existingUser) {
-        throw new BadRequestError('User already exists with the provided email.', { email });
-      }
-
-      const userRole = await this.roleRepository.findOne({ where: { id: roleId } });
-      if (!userRole) {
-        throw new NotFoundError('Role not found.', { roleId });
-      }
-
-      const user = this.userRepository.create({ ...userData, role: userRole });
-      const newUser = await this.userRepository.save(user);
-      const userDto = plainToInstance(UserDto, newUser, { excludeExtraneousValues: true });
-
-      return { user: userDto, result: UserResultMessage.SUCCEED };
-    } catch (error) {
-      if (error instanceof BadRequestError) {
-        throw error;
-      }
-
-      throw new DatabaseConnectionError({ operation: 'save', error });
+    const existingUser = await this.userRepository.findOne({ where: { email }, withDeleted: true });
+    if (existingUser) {
+      throw new BadRequestError('User already exists with the provided email.', { email });
     }
+
+    const userRole = await this.roleRepository.findOne({ where: { id: roleId } });
+    if (!userRole) {
+      throw new NotFoundError('Role not found.', { roleId });
+    }
+
+    const user = this.userRepository.create({ ...userData, role: userRole });
+    const newUser = await this.userRepository.save(user);
+    const userDto = plainToInstance(UserDto, newUser, { excludeExtraneousValues: true });
+
+    return { user: userDto, result: UserResultMessage.SUCCEED };
   }
 
   @EventPublisherDecorator({ keyTemplate: REDIS_CACHE_KEYS.USER_GET_LIST, event: EVENTS.USER_UPDATED })
   async update (id: number, userData: UpdateUserArgs): Promise<UserResultsDto> {
-    try {
-      const userToBeUpdated = await this.userRepository.findOneBy({ id });
-      if (!userToBeUpdated) {
-        throw new NotFoundError(`User with ID ${id} not found.`, { id });
-      }
-
-      if (userData?.email) {
-        const existingUser = await this.userRepository.findOne({ where: { email: userData?.email }, withDeleted: true });
-        if (existingUser) {
-          throw new BadRequestError('User already exists with the provided email.', { email: userData.email });
-        }
-      }
-
-      Object.assign(userToBeUpdated, userData);
-      const updatedUser = await this.userRepository.save(userToBeUpdated);
-      const userDto = plainToInstance(UserDto, updatedUser, { excludeExtraneousValues: true });
-
-      return { user: userDto, result: UserResultMessage.SUCCEED };
-    } catch (error) {
-      if (error instanceof NotFoundError || error instanceof BadRequestError) {
-        throw error;
-      }
-
-      throw new DatabaseConnectionError({ operation: 'save', error });
+    const userToBeUpdated = await this.userRepository.findOneBy({ id });
+    if (!userToBeUpdated) {
+      throw new NotFoundError(`User with ID ${id} not found.`, { id });
     }
+
+    if (userData?.email) {
+      const existingUser = await this.userRepository.findOne({ where: { email: userData?.email }, withDeleted: true });
+      if (existingUser) {
+        throw new BadRequestError('User already exists with the provided email.', { email: userData.email });
+      }
+    }
+
+    Object.assign(userToBeUpdated, userData);
+    const updatedUser = await this.userRepository.save(userToBeUpdated);
+    const userDto = plainToInstance(UserDto, updatedUser, { excludeExtraneousValues: true });
+
+    return { user: userDto, result: UserResultMessage.SUCCEED };
   }
 
   @EventPublisherDecorator({ keyTemplate: REDIS_CACHE_KEYS.USER_GET_LIST, event: EVENTS.USER_DELETED })
   async delete ({ id }: DeleteUserArgs): Promise<UserResultsDto> {
-    try {
-      const existingUser = await this.userRepository.findOne({ where: { id } });
-      if (!existingUser) {
-        throw new NotFoundError(`User with ID ${id} not found.`, { id });
-      }
-
-      await this.userRepository.softDelete(id);
-      return { result: UserResultMessage.SUCCEED };
-    } catch (error) {
-      if (error instanceof NotFoundError) {
-        throw error;
-      }
-
-      throw new DatabaseConnectionError({ operation: 'softDelete', error });
+    const existingUser = await this.userRepository.findOne({ where: { id } });
+    if (!existingUser) {
+      throw new NotFoundError(`User with ID ${id} not found.`, { id });
     }
+
+    await this.userRepository.softDelete(id);
+    return { result: UserResultMessage.SUCCEED };
   }
 }
